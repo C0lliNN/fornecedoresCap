fce24823ccc23724d18e5f2d8f2c1522cec1ee11
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2023-11-08 13:20:16 -0300

    Fix config folder

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
+
 class Database {
   constructor() {
-    this.user = process.env.DB_USER;
     this.uri = process.env.DB_URI;
-    this.pass = process.env.DB_PASS;
   }
 
   start = async () => {
-    await mongoose
-      .connect(`mongodb+srv://${this.user}:${this.pass}@${this.uri}`)
-      .then(() => console.log("Concetado com o banco de dados"))
-      .catch((error) =>
-        console.log("Erro ao conectar o banco de dados" + error)
-      );
+    try {
+      await mongoose.connect(this.uri);
+      console.log("Concetado com o banco de dados")
+    } catch(error) {
+      console.log("Erro ao conectar o banco de dados" + error)
+    }
   };
 }
 
